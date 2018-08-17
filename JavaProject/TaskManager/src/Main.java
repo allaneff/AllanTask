001c5c578350f46beb16beeb9997f69b8b2f94df
Allan Jhony Effting <allaneff94@gmail.com>
2018-08-16 22:58:48 -0300

    new

--- JavaProject/TaskManager/src/Main.java
+++ JavaProject/TaskManager/src/Main.java
@@ -1,2 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
+    public static void main(String[]args){
+        String stringConexao = "jdbc:mysql://localhost:3306/?";
+        String nomeBancoDeDados = ""; // nome do banco de dados que foi criado no sqlworband -- baixar mysql-java-8.0.12, pasta libs
+        String usuarioBD = "";
+        String senhaBD = "";
+        try{
+            Connection conn = DriverManager
+                    .getConnection(String.format(stringConexao, nomeBancoDeDados),usuarioBD,senhaBD);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
